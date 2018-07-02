fe683a26d72963a1d09384f617ea7572fea8d5b5
Unknown <lahirusabe15@gmail.com>
2018-07-02 14:41:36 +0530

    Payment report generates

--- angular-front/src/app/components/adminfines/adminfines.component.ts
+++ angular-front/src/app/components/adminfines/adminfines.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { FlashMessage } from 'angular-flash-message';
+import * as jsPDF from 'jspdf';
+import 'jspdf-autotable';
 
 @Component({
   selector: 'app-adminfines',
@@ -49,6 +51,7 @@ export class AdminfinesComponent implements OnInit {
   }
 
   pay(item:any){
+
     this.auth.updateUnpaid(item).subscribe(res=>{
        console.log(res);
        this.flashMessage.success('Paid details updated..', {
@@ -59,6 +62,39 @@ export class AdminfinesComponent implements OnInit {
         navigate: '/'
       });
      });
+  }
+
+  dwnload(){
+
+    var today = new Date();
+    var today_dy = (today.getDate()+1), today_mnt = (today.getMonth()+1), today_yr = (today.getFullYear()+1);
+
+    var date = today_yr + "-" + today_mnt + "-" + today_dy;
+    //var datenm = today_yr + "" + today_mnt + "-" + today_dy;
+
+    var rows =[];
+
+    var i=0;
+    for(let x of this.rslvdData){
+      var insiderow =[];
+      insiderow.push(this.rslvdData[i].username);
+      insiderow.push(this.rslvdData[i].fine);
+      insiderow.push(this.rslvdData[i].status);
+
+      i=i+1;
+      rows.push(insiderow);
+    }
+    
+  
+    
+
+    const doc = new jsPDF();
+    //var cols = ["paying id", "borrow id", "username", "fine"];
+    let cols = ["username", "fine", "status"];
+
+    doc.autoTable(cols,rows);    
+    doc.text(10, 10, "Fine Details - " + date);
+    doc.save(date+'.pdf');
   }
 
 }
